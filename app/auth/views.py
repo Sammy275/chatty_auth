ca83768fa81c138978c90d7e4d528877dd6dc5a2
Saim Irfan <samfan95@gmail.com>
2020-11-14 00:35:49 +0500

    added form validation functionality and appropriate rendering

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, flash
 from . import auth
 from app.auth.forms import RegisterForm
 
@@ -6,7 +6,9 @@ from app.auth.forms import RegisterForm
 def login():
     return render_template('auth/login.html')
 
-@auth.route('/register')
+@auth.route('/register', methods=["GET", "POST"])
 def register():
     form = RegisterForm()
+    if form.validate_on_submit():  
+        return "nice"
     return render_template('auth/register.html', form=form)
