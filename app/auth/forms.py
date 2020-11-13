5d8f90c93a28815f7bd714412b64e8da00d55cc6
Saim Irfan <samfan95@gmail.com>
2020-11-13 23:14:17 +0500

    added submit field

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SelectField
+from wtforms import StringField, PasswordField, SelectField, SubmitField
 from wtforms.validators import Length, Email, DataRequired, Regexp, EqualTo
 from wtforms.fields.html5 import EmailField
 import datetime
@@ -12,3 +12,4 @@ class RegisterForm(FlaskForm):
     day = SelectField("Day", choices=[(i, i) for i in range(1, 32)])
     month = SelectField("Month", choices=[(1, 'Jan'), (2, 'Feb'), (3, 'Mar'), (4, 'Apr'), (5, 'May'), (6, 'Jun'), (7, 'Jul'), (8, 'Aug'), (9, 'Sep'), (10, 'Oct'), (11, 'Nov'), (12, 'Dec')])
     year = SelectField("Year", choices=[(i, i) for i in range(1900, datetime.date.today().year + 1)])
+    submit = SubmitField("Sign Up")
