5c3edc9203aeba18f85dd36f515e195dac01982d
Saim Irfan <samfan95@gmail.com>
2020-11-19 17:52:30 +0500

    Handeling login requests and user authentication

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,13 +1,21 @@
 from flask import render_template, request, flash, redirect, url_for
-from flask_login import logout_user, login_required
+from flask_login import logout_user, login_required, login_user
 from . import auth
-from app.auth.forms import RegisterForm
+from app.auth.forms import RegisterForm, LoginForm
 from ..models import User
 from app import db
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email = form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            flash('You have been logged in')
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid Username or Password')
+    return render_template('auth/login.html', form=form)
 
 @auth.route('/register', methods=["GET", "POST"])
 def register():
