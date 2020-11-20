b23212c95a780da15bdba6cedbd26814ddfe0ee7
Saim Irfan <samfan95@gmail.com>
2020-11-20 15:15:12 +0500

    Rendering 404 page

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,5 +1,6 @@
 from . import main
+from flask import render_template
 
 @main.app_errorhandler(404)
 def page_not_found(e):
-    return "Error 404: page not found"
+    return render_template('main/404_error.html')
