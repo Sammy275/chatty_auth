5eadfc395e72ea528e03e031a922c5625e28b8b2
Saim Irfan <samfan95@gmail.com>
2020-11-22 15:42:25 +0500

    Setting up email sending functionality

--- config.py
+++ config.py
@@ -9,6 +9,11 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:mysqliscrazyman@localhost/chatty'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'samfan95@gmail.com'
+    MAIL_PASSWORD = 'ilovepakistan'
 
 class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:mysqliscrazyman@localhost/chatty_test'
