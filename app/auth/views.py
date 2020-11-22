3ca05574d0aa41bc9eb3f40285a851654105aab8
Saim Irfan <samfan95@gmail.com>
2020-11-22 17:31:38 +0500

    Set up user account confirmation and email verification

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,7 @@ from . import auth
 from app.auth.forms import RegisterForm, LoginForm
 from ..models import User
 from app import db
+from ..emails import send_email
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -28,9 +29,45 @@ def register():
         user = User(email=form.email.data, name=form.name.data, password=form.password.data, user_age=form.year.data)
         db.session.add(user)
         db.session.commit()
-        flash("You are registered. Now you can log in to your account")
-        return redirect(url_for('auth.login'))
+        token = user.generate_confirmation_token()
+
+        send_email(user.email, 'Confirm Your Account', 'auth/email/confirm', user=user, token=token)
+
+        flash("A confirmation email has been sent to your email. Please check it")
+        return redirect(url_for('main.index'))
+
     return render_template('auth/register.html', form=form)
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated and not current_user.confirmed and request.endpoint[:5] != 'auth.':
+        return redirect(url_for('auth.unconfirmed'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect('main.index')
+    return render_template('auth/unconfirmed.html')
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    
+    if current_user.confirm(token):
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash("The confirmation link is invalid or has expired")
+    return redirect(url_for('main.index'))
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm Your Account', 'auth/email/confirm', user=current_user, token=token)
+    flash('A new confirmation email has been sent to you by email.')
+    return redirect(url_for('main.index'))
 
 @auth.route('/logout')
 @login_required
