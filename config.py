1aa4f6880e40d4933fa4e83b59251bd12892a5e6
Saim Irfan <samfan95@gmail.com>
2020-11-22 17:45:51 +0500

    I dont know what this is

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class DevelopmentConfig(Config):
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') 'zdsvxnorbtaehnuw'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
 class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:mysqliscrazyman@localhost/chatty_test'
