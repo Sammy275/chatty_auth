8a575bf62bddb03dc00cc072b76f009348f84034
Saim Irfan <samfan95@gmail.com>
2020-11-22 17:49:53 +0500

    fixed bad english

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -7,7 +7,7 @@ from ..models import User
 from wtforms import ValidationError
 
 class RegisterForm(FlaskForm):
-    name = StringField("Username", validators=[DataRequired(), Regexp('^[A-Za-z][A-Za-z0-9_]*$', 0, "Username must have only letters, numbers, dots or underscored")])
+    name = StringField("Username", validators=[DataRequired(), Regexp('^[A-Za-z][A-Za-z0-9_]*$', 0, "Username must have only letters, numbers, dots or underscores")])
     email = EmailField("Email", validators=[Email(), DataRequired(), Length(1, 64)])
     password = PasswordField("Password", validators=[DataRequired(), EqualTo('confirm_pass', message='Password must match')])
     confirm_pass = PasswordField("Confirm Password", validators=[DataRequired()])
