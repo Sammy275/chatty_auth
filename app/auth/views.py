edf1928a8e3bd6db07713a88c257115efc7dc6d8
Saim Irfan <samfan95@gmail.com>
2020-11-22 17:32:21 +0500

    Set up user account confirmation and email verification

--- app/auth/views.py
+++ app/auth/views.py
@@ -38,17 +38,6 @@ def register():
 
     return render_template('auth/register.html', form=form)
 
-@auth.before_app_request
-def before_request():
-    if current_user.is_authenticated and not current_user.confirmed and request.endpoint[:5] != 'auth.':
-        return redirect(url_for('auth.unconfirmed'))
-
-@auth.route('/unconfirmed')
-def unconfirmed():
-    if current_user.is_anonymous or current_user.confirmed:
-        return redirect('main.index')
-    return render_template('auth/unconfirmed.html')
-
 @auth.route('/confirm/<token>')
 @login_required
 def confirm(token):
@@ -60,6 +49,17 @@ def confirm(token):
     else:
         flash("The confirmation link is invalid or has expired")
     return redirect(url_for('main.index'))
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated and not current_user.confirmed and request.endpoint[:5] != 'auth.':
+        return redirect(url_for('auth.unconfirmed'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
 
 @auth.route('/confirm')
 @login_required
