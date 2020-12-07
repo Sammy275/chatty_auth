ad1078f0dc2168fff6af722876ab2231dac4c5fc
Saim Irfan <samfan95@gmail.com>
2020-12-07 15:43:22 +0500

    added a route handler to view user's profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, abort
 from . import main
+from ..models import User
 
 @main.route('/')
 def index():
@@ -8,3 +9,10 @@ def index():
 @main.route('/about')
 def about():
     return render_template('main/about.html')
+
+@main.route('/user/<username>')
+def profile(username):
+    user = User.query.filter_by(name=username).first()
+    if not user:
+        abort(404)
+    return render_template('main/profile.html', user=user)
