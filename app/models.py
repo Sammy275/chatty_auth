98227cc0c118c575d9871144d4b7a59595a9ae1d
Saim Irfan <samfan95@gmail.com>
2020-12-07 15:46:43 +0500

    removed a comment

--- app/models.py
+++ app/models.py
@@ -20,7 +20,6 @@ class User(db.Model, UserMixin):
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
     is_administrator = db.Column(db.Boolean, default=False)
-    # role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
 
 
     # Password setting
