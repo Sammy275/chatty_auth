8d39006b6bd08ed992cbd49191afd4c72e12e760
Saim Irfan <samfan95@gmail.com>
2020-12-15 12:30:19 +0500

    fixed some issues during the startup

--- setup.cmd
+++ setup.cmd
@@ -1,8 +1,9 @@
-start "" "http://www.google.com"
+start "" "http://localhost:5000"
 start cmd /k git status
 start cmd /k psql -U postgres
-start cmd /c code .
+CALL code .
 CALL env\Scripts\activate.bat
 set FLASK_APP=manage.py
 set FLASK_DEBUG=1
+CALL flask run
 cmd /k
