f8668c2ef176b4167d972404ee3c168d0ad8ecad
Saim Irfan <samfan95@gmail.com>
2020-12-15 15:38:45 +0500

    added new .gitignore file

--- setup.cmd
+++
@@ -1,9 +0,0 @@
-start "" "http://localhost:5000"
-start cmd /k git status
-start cmd /k psql -U postgres
-CALL code .
-CALL env\Scripts\activate.bat
-set FLASK_APP=manage.py
-set FLASK_DEBUG=1
-CALL flask run
-cmd /k
